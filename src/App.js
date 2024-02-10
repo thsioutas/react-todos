147e2fd7460bb88ca429c37c8203af31af3c1c95
Thomas Sioutas <thsioutas@gmail.com>
2024-02-10 18:57:20 +0200

    Introduce Todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+// import useState hook which will add "state" functionality in our component 
+import React, {useState} from 'react';
 import './App.css';
+import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
+
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
+  // useState returns a stateful value (todos) and
+  // a function (setTodos) to update this sateful value
+  const [todos, setTodos] = useState([
+    {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
+    {rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
+    {rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'User One'},
+    {rowNumber: 4, rowDescription: 'Charge phone battery', rowAssigned: 'User One'},
+  ]
+  )
+
+  const addTodo = (description, assigned) => {
+    // console.log('Our addTodo btn has been clicked')
+    let rowNumber = 0;
+    if (todos.length > 0) {
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    } else {
+      rowNumber = 1;
+    }
+      const newTodo = {
+        rowNumber: rowNumber,
+        rowDescription: description,
+        rowAssigned: assigned,
+      };
+      
+      setTodos(todos => [...todos, newTodo]);
+      // console.log(todos);
+  }
+
+  const deleteTodo = (deleteTodoRowNumber) => {
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== deleteTodoRowNumber;
+    });
+    setTodos(filtered)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mt-5 container'>
+      <div className="card">
+        <div className="card-header">
+          Your Todo's
+        </div>
+        <div className="card-body">
+          <TodoTable todos={todos} deleteTodo={deleteTodo}/>
+          <button onClick={() => setShowAddTodoForm(!showAddTodoForm)} className='btn btn-primary'>
+            {showAddTodoForm ? 'Close New Todo' : 'New Todo'}
+          </button>
+        {showAddTodoForm &&
+          <NewTodoForm addTodo={addTodo}/>
+        }
+        </div>
+      </div>
     </div>
   );
 }
